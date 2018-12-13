f9cc2e36ba3ebae941b2fc01a9b526a1c007cb19
Martino Facchin <m.facchin@arduino.cc>
2018-12-13 16:29:43 +0100

    Add very rough build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Build for every architecture and emit json and shasums
+
+rm -rf distrib
+
+# TODO: replace me with proper go modules stuff
+export GOPATH=$PWD
+
+export CGO_ENABLED=0
+GOOS=linux GOARCH=amd64 go build -o distrib/linux64/mdns-discovery
+GOOS=linux GOARCH=386 go build -o distrib/linux32/mdns-discovery
+GOOS=linux GOARCH=arm go build -o distrib/linuxarm/mdns-discovery
+GOOS=linux GOARCH=arm64 go build -o distrib/linuxarm64/mdns-discovery
+GOOS=windows GOARCH=386 GO386=387 go build -o distrib/windows/mdns-discovery.exe
+CGO_ENABLED=1 CC=o64-clang GOOS=darwin GOARCH=amd64 go build -ldflags="-extldflags=-mmacosx-version-min=10.9" -o distrib/darwin/mdns-discovery
+
+cd distrib
+zip -r ../mdns-discovery-${VERSION}.zip *
+cd ..
+
+shasum mdns-discovery-${VERSION}.zip
+sha256sum mdns-discovery-${VERSION}.zip
+ls -la mdns-discovery-${VERSION}.zip
