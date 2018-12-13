4e40c4553471ebcf629b8661837dcf73bed0cd57
Martino Facchin <m.facchin@arduino.cc>
2018-12-13 16:30:57 +0100

    Avoid using cgo for darwin build

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ GOOS=linux GOARCH=386 go build -o distrib/linux32/mdns-discovery
 GOOS=linux GOARCH=arm go build -o distrib/linuxarm/mdns-discovery
 GOOS=linux GOARCH=arm64 go build -o distrib/linuxarm64/mdns-discovery
 GOOS=windows GOARCH=386 GO386=387 go build -o distrib/windows/mdns-discovery.exe
-CGO_ENABLED=1 CC=o64-clang GOOS=darwin GOARCH=amd64 go build -ldflags="-extldflags=-mmacosx-version-min=10.9" -o distrib/darwin/mdns-discovery
+GOOS=darwin GOARCH=amd64 go build -o distrib/darwin/mdns-discovery
 
 cd distrib
 zip -r ../mdns-discovery-${VERSION}.zip *
